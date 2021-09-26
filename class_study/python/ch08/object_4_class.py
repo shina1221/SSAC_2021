5caa303f9744b09fa4d6e62f4ab23ab3bb7dea30
shinnajeoung <shina1221@naver.com>
2021-09-26 15:46:45 +0900

    Create object_4_class.py

---
+++ class_study/python/ch08/object_4_class.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name, korean, math, english, science):
+        self.name = name,
+        self.korean = korean,
+        self.math = math,
+        self.english = english,
+        self.science = science
+
+    
