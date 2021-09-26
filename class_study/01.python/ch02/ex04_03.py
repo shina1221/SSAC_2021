73584578b8a18bc046944757cf30457684b15168
shinnajeoung <shina1221@naver.com>
2021-09-26 18:14:53 +0900

    Create ex04_03.py

---
+++ class_study/01.python/ch02/ex04_03.py
@@ -0,0 +1,21 @@
+#105P
+a=input("> 1번째 숫자: ")
+b=input("> 2번째 숫자: ")
+print()
+
+print("{ } + { } = { }".format(a,b,int(a)+int(b)))
+
+string = "hello"
+
+#string.upper()를 실행하고, string 출력
+string.upper()
+print("A 지점:", string)
+
+#string.upper() 실행
+print("B 지점:", string.upper())
+
+#불린에서 대문자가 소문자보다 작다는 관계를 가짐
+#문자비교
+#알파벳 가나다 순
+#앞에가 똑같다면 뒤에 더 붙어있는 것이 더 큼
+#a1은 a1b보다 작음
