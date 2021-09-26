8993f464979e51a333e7a0d206912696f25a2767
shinnajeoung <shina1221@naver.com>
2021-09-26 18:33:30 +0900

    Create ininstance.py

---
+++ class_study/01.python/ch08/ininstance.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, name, korean, math, english, science):
+        self.name = name,
+        self.korean = korean,
+        self.math = math,
+        self.english = english,
+        self.science = science
