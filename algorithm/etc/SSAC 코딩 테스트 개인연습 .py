bbf3c70dc04ccec6abeb874ef2e571a367d084c9
shinnajeoung <shina1221@naver.com>
2021-11-06 14:07:19 +0900

    Update SSAC 코딩 테스트 개인연습 .py

--- algorithm/etc/SSAC 코딩 테스트 개인연습 .py
+++ algorithm/etc/SSAC 코딩 테스트 개인연습 .py
@@ -148,4 +148,51 @@ a.sub()
 a.div()
 
 #클래스 상속
+class MoreFourCal(FourCal):
+    pass
 
+a = MoreFourCal(3,3)
+a.add()
+
+#메서드 오버라이딩
+"""
+부모 클래스(상속한 클래스)에 있는 메서드를 동일한 이름으로 다시 만드는 것을 
+메서드 오버라이딩(Overriding, 덮어쓰기)이라고 한다. 
+이렇게 메서드를 오버라이딩하면 부모클래스의 메서드 대신 오버라이딩한 메서드가 호출된다.
+"""
+a= FourCal(4,0)
+a.div() #오류남 정상
+
+class safeFourCal(FourCal):
+    def div(self):
+        if self.second==0:
+            return 0
+        else:
+            return self.first/self.second
+
+a = safeFourCal(4,0)
+a.div()
+
+#클래스 변수
+class Family:
+    lastname='kim'
+
+Family.lastname
+
+"""
+클래스 변수 값을 변경했더니 클래스로 만든 객체의 lastname 값도 모두 변경된다는 것을
+확인할 수 있다. 즉 클래스 변수는 클래스로 만든 모든 객체에 공유된다는 특징이 있다.
+"""
+Family.lastname = '박'
+Family.lastname
+
+#거품정렬
+test =[5,4,3,2,1]
+print(test)
+for i in range(len(test)):
+    for j in range(len(test)-i-1):
+        if test[j]> test[j+1]:
+            test[j],test[j+1]=test[j+1],test[j]
+        else:
+            pass
+        print(test)
