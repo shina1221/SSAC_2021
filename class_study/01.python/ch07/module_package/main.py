2bbfeec571a3528cbaf27ed1ae5b7eb540996dc5
shina1221 <38153316+shina1221@users.noreply.github.com>
2023-03-05 22:05:18 +0900

    Delete class_study directory

--- class_study/01.python/ch07/module_package/main.py
+++
@@ -1,5 +0,0 @@
-import test_packge.module_a as a
-import test_packge.module_b as b
-
-print(a.variable_a)
-print(a.variable_b)
--- class_study/01.python/ch07/test_module.py
+++
@@ -1,6 +0,0 @@
-#test_module
-name = 'Iceman'
-llist = [1,2,3,4,5]
-def ssum(*nnums):
-    return sum(nnums)
-
